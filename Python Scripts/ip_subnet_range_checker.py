ab0ab02779fca3b339634a6ab79842f34dbdd011
Ayush Dhoot <101291613+Ayush4221@users.noreply.github.com>
2024-10-07 23:33:24 +0530

    final
    removed everything

--- Python Scripts/ip_subnet_range_checker.py
+++ Python Scripts/ip_subnet_range_checker.py
@@ -1,16 +1,11 @@
 import ipaddress 
-# This import allows for easy manipulation of network addresses, subnets, etc ...
 
 def range_checker(ip: str, subnet: str ) -> bool:
-    '''
-    This will take two parameters, an ip address and a subnet mask.
-    It will return True if the ip address is within the subnet range, and False otherwise.
-    We Will assume network addresses start at 0
-    in form range_checker('192.168.10.0', '255.255.255.0'))
-    '''
-    network = ipaddress.ip_network(f'{ip}/{subnet}', strict=False) # Create a network object from the subnet
+   
+    
+    network = ipaddress.ip_network(f'{ip}/{subnet}', strict=False) 
     print(network)
-    return ipaddress.ip_address(ip) in network # Check if the ip address is in the network
+    return ipaddress.ip_address(ip) in network 
 
-print(range_checker('192.168.10.0', '255.255.255.0')) # True
+print(range_checker('192.168.10.0', '255.255.255.0')) 
     
